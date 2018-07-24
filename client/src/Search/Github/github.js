f4a6eeff1c0985780f60fc5d6c5578246530b385
glearner <gkvom@outlook.com>
2018-07-24 14:01:01 +0530

    lastest_build

--- client/src/Search/Github/github.js
+++ client/src/Search/Github/github.js
@@ -4,20 +4,26 @@ const Github =(props)=>{
 
         return(
             <div className="jumbotron">
+                <div className="float-left">
+                    <img
+                     style={{width : '5rem'}}
+                     alt= {props.name}
+                     src={props.imageUrl}/>
+                    </div>
                 <div className="row">
-                        <div className="float-left">
-                            <img
-                            style={{width : '5rem'}}
-                            alt= {props.name}
-                            src={props.imageUrl}/>
-                            </div>
-                        <div>
-                        <p> {props.name}</p> 
-                        </div>      
-                </div>  
+                <div className="col-md">
+                    </div>
+                <div className="col-md">
+                <p> {props.name}</p> 
+                    </div>
+                <div className="col-md">
+                    </div>
+                
+                </div>        
                 <div className="row">
-                        <div className="col-md-2 col-sm-2">
+                        <div className="col-md-2 col-xs-2">
                             </div>
+                    
                     </div>
             </div>
         )
